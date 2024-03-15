28fa850c70a107a5a0d058eb9e2750178db701e9
can caliskan <podcastwerv2017@gmail.com>
2024-03-15 15:26:37 +0200

    image quantity selector

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const generateFrom = document.querySelector(".generate-form");
+const imageGallery = document.querySelector(".image-gallery");
 
 const handleFormSubmission = (e) => {
     e.preventDefault();
@@ -6,7 +7,16 @@ const handleFormSubmission = (e) => {
     const userPrompt = e.srcElement[0].value;
     const userImgQuantity = e.srcElement[1].value;
 
-    console.log(userPrompt, userImgQuantity);
+    const imgCardMarkup = Array.from({length: userImgQuantity}, () => 
+    `<div class="img-card loading">
+        <img src="images/loader.gif" alt="img">
+        <a href="#" class="download-btn">
+            <img src="images/download.svg" alt="download icon">
+        </a>
+     </div>`
+    ).join("");
+
+    imageGallery.innerHTML = imgCardMarkup;
 }
 
 generateFrom.addEventListener("submit", handleFormSubmission);
